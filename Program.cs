a7d691cb1a0a1da413b5c3c7d74ecaad23c4758d
maheir <maheirne@gmail.com>
2018-12-17 15:43:01 +0100

    VARIABLE EXERCICE 4

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mr = "Monsieur";
+            string name = "Vincent";
+            string num = "1";
+
+            Console.WriteLine("Bonjour, " + mr+" " + name + " vous etes venu nous rendre visite "+num+ " fois");
+
+        }
+    }
+}
